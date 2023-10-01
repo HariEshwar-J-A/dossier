ff2aa8b919356b956b8fc7e082c815f3f82932c6
Harieshwar J A <harirmdeee@gmail.com>
2023-10-01 15:05:42 -0400

    feat: :card_file_box: add updated data for projects section

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -2,7 +2,7 @@ export const ProjectData = [
     {
         id: 1,
         title: "Weather Monitoring App",
-        about: "This site was hosted using real-time API for getting weather data across major cities across the world.",
+        about: "This site is hosted using real-time API for getting weather data across major cities across the world.",
         tags: ["HTML5", "CSS3", "JavaScript", "NodeJS"],
         demo: "https://harieshwar-j-a.github.io/hari.github.io/",
         repository: "https://github.com/HariEshwar-J-A/hari.github.io",
@@ -11,7 +11,7 @@ export const ProjectData = [
     {
         id: 2,
         title: "Word Scramble Game",
-        about: "In laboris consectetur irure laborum tempor magna proident occaecat. Aliqua quis laboris minim laboris Lorem mollit cillum ad duis consectetur ipsum velit irure. Adipisicing cillum anim pariatur ex. Velit nulla deserunt esse reprehenderit elit. Quis sunt labore est nostrud qui voluptate esse in eu. Non aute incididunt amet ex laboris Lorem eu id deserunt ullamco Lorem Lorem officia. Pariatur veniam anim in mollit occaecat aliquip Lorem.",
+        about: "This site is a word scramble game where we can play against real time players to score the highest.",
         tags: ["HTML5", "CSS3", "JavaScript", "NodeJS", "Web Sockets"],
         demo: "https://word-scramble-game.glitch.me/",
         repository: "https://glitch.com/~word-scramble-game",
@@ -20,7 +20,7 @@ export const ProjectData = [
     {
         id: 3,
         title: "Chat Box App",
-        about: "In laboris consectetur irure laborum tempor magna proident occaecat. Aliqua quis laboris minim laboris Lorem mollit cillum ad duis consectetur ipsum velit irure. Adipisicing cillum anim pariatur ex. Velit nulla deserunt esse reprehenderit elit. Quis sunt labore est nostrud qui voluptate esse in eu. Non aute incididunt amet ex laboris Lorem eu id deserunt ullamco Lorem Lorem officia. Pariatur veniam anim in mollit occaecat aliquip Lorem.",
+        about: "This site is a sample chat box used to explain the simple yet effective capabilities of using web sockets.",
         tags: ["HTML5", "CSS3", "JavaScript", "NodeJS", "Web Sockets"],
         demo: "https://chat-box-web.glitch.me/",
         repository: "https://glitch.com/~chat-box-web",
