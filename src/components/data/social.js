8e4b6073d9b6f7aad14f10bb9bff99cce1b73f31
Harieshwar J A <harirmdeee@gmail.com>
2025-03-06 15:51:01 -0500

    feat: :card_file_box: update social & work data with assets
    Add experience description and logo for the experience in freelance web development and soti, alongside updating instagram username url

--- src/components/data/social.js
+++ src/components/data/social.js
@@ -1,27 +1,27 @@
 export const SocialData = [
-    {
-        platform: "Instagram",
-        link: "https://www.instagram.com/uzumaki_harieshwar/",
-        icon: require("../../assets/icons/instagram.svg").default,
-    },
-    {
-        platform: "Github",
-        link: "https://github.com/HariEshwar-J-A",
-        icon: require("../../assets/icons/github.svg").default,
-    },
-    {
-        platform: "Facebook",
-        link: "https://www.facebook.com/hari.eshwar.505",
-        icon: require("../../assets/icons/facebook.svg").default,
-    },
-    {
-        platform: "LinkedIn",
-        link: "https://www.linkedin.com/in/harieshwar-jagan-abirami-325961171/",
-        icon: require("../../assets/icons/linkedin.svg").default,
-    },
-    {
-        platform: "Twitter",
-        link: "https://twitter.com/hariSpartan_1",
-        icon: require("../../assets/icons/twitter.svg").default,
-    },
+  {
+    platform: "Instagram",
+    link: "https://www.instagram.com/harieshwar_j_a/",
+    icon: require("../../assets/icons/instagram.svg").default,
+  },
+  {
+    platform: "Github",
+    link: "https://github.com/HariEshwar-J-A",
+    icon: require("../../assets/icons/github.svg").default,
+  },
+  {
+    platform: "Facebook",
+    link: "https://www.facebook.com/hari.eshwar.505",
+    icon: require("../../assets/icons/facebook.svg").default,
+  },
+  {
+    platform: "LinkedIn",
+    link: "https://www.linkedin.com/in/harieshwar-jagan-abirami-325961171/",
+    icon: require("../../assets/icons/linkedin.svg").default,
+  },
+  {
+    platform: "Twitter",
+    link: "https://twitter.com/hariSpartan_1",
+    icon: require("../../assets/icons/twitter.svg").default,
+  },
 ];
